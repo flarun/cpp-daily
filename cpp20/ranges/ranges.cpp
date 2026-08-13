ec2f84bbfc5e9fedba9e4a2b4b0d5e3f947e7643
flarun <69197787+flarun@users.noreply.github.com>
2026-08-13 17:19:20 +0200

    feat: ranges

---
+++ cpp20/ranges/ranges.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <ranges>
+
+int main()
+{
+  std::vector<int> numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+  // take numbers, filter out odd ones, square the evens
+  auto results = numbers | std::views::filter([](int n)
+                                              { return n % 2 == 0; }) |
+                 std::views::transform([](int n)
+                                       { return n * n; });
+
+  for (int n : results)
+  {
+    std::cout << n << " ";
+  }
+}
